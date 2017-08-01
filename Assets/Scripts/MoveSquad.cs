da0d676341d5840d3d206ae7885a1b6bbdbdfc02
Javier Bullrich <javierbullrich@gmail.com>
2017-08-01 18:37:53 -0300

    Added a scene to move a squad

---
+++ Assets/Scripts/MoveSquad.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Blue.Pathfinding;
+
+// by @Bullrich
+
+namespace rts
+{
+    public class MoveSquad : MonoBehaviour
+    {
+        public Unit[] units;
+        private Vector3[] offsets;
+
+        void Start()
+        {
+            offsets = new Vector3[units.Length];
+            for (int i = 0; i < units.Length; i++)
+            {
+                offsets[i] = units[i].transform.position - units[0].transform.position;
+            }
+        }
+
+        public void MoveToPoint(Vector3 point)
+        {
+            for (int i = 0; i < units.Length; i++)
+            {
+                units[i].NavigateToPoint(units[i].transform.position, point + offsets[i]);
+            }
+        }
+
+        private void CheckClick()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    MoveToPoint(hit.point);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            CheckClick();
+        }
+    }
+}
---
+++ Assets/Scripts/Soldier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Blue.Pathfinding;
+
+// by @Bullrich
+
+namespace rts
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Soldier : Unit
+    {
+        CharacterController _controller;
+        private void Start()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+        public override void MoveTo(Vector3 targetPos)
+        {
+            Vector3 offset = targetPos - transform.position;
+            offset = offset.normalized * speed;
+            _controller.Move(offset * Time.deltaTime);
+        }
+
+    }
+}
